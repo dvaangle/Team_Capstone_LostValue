557be6810af3e119c9ae7c1f78496b76bd216481
뚱별 <51257037+dvaangle@users.noreply.github.com>
2023-09-22 00:53:48 +0900

    camera offset
    버그수정

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float moveSpeed; // 움직임 속도
     private float moveHorizontal;
+    private bool facingRight = true;
 
     [Header("Jump System")]
     [SerializeField]
@@ -26,7 +27,7 @@ public class PlayerController : MonoBehaviour
     bool isJumping;
     float jumpCounter;
 
-    public Transform groundCheck; 
+    public Transform groundCheck;
     public LayerMask groundLayer;
     #endregion
 
@@ -39,17 +40,17 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         moveHorizontal = Input.GetAxisRaw("Horizontal");
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded())
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
         {
             rb2D.velocity = new Vector2(rb2D.velocity.x, jumpForce);
             isJumping = true;
             jumpCounter = 0;
         }
 
-        if(rb2D.velocity.y > 0 && isJumping)
+        if (rb2D.velocity.y > 0 && isJumping)
         {
             jumpCounter += Time.deltaTime;
-            if(jumpCounter > jumpTime)
+            if (jumpCounter > jumpTime)
             {
                 isJumping = false;
             }
@@ -57,7 +58,7 @@ public class PlayerController : MonoBehaviour
             float t = jumpCounter / jumpTime;
             float currentJumpM = jumpMultiplier;
 
-            if(t > 0.5f)
+            if (t > 0.5f)
             {
                 currentJumpM = jumpMultiplier * (1 - t);
             }
@@ -65,18 +66,18 @@ public class PlayerController : MonoBehaviour
             rb2D.velocity += vecGravity * currentJumpM * Time.deltaTime;
         }
 
-        if(Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.Space))
         {
             isJumping = false;
             jumpCounter = 0;
 
-            if(rb2D.velocity.y > 0)
+            if (rb2D.velocity.y > 0)
             {
                 rb2D.velocity = new Vector2(rb2D.velocity.x, rb2D.velocity.y * 0.6f);
             }
         }
 
-        if(rb2D.velocity.y < 0)
+        if (rb2D.velocity.y < 0)
         {
             rb2D.velocity -= vecGravity * fallMultiplier * Time.deltaTime;
         }
@@ -84,22 +85,31 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(moveHorizontal > 0.1f || moveHorizontal < -0.1f)
+        if (moveHorizontal > 0.1f || moveHorizontal < -0.1f)
         {
             rb2D.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse);
         }
-        if(moveHorizontal > 0)
+        if (moveHorizontal > 0 && !facingRight)
         {
-            gameObject.transform.localScale = new Vector3(1, 1, 1);
+            Flip();
         }
-        if(moveHorizontal < 0)
+        if (moveHorizontal < 0 && facingRight)
         {
-            gameObject.transform.localScale = new Vector3(-1, 1, 1);
+            Flip();
         }
     }
 
     bool isGrounded()
     {
-        return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1f, 0.06f), CapsuleDirection2D.Horizontal, 0, groundLayer);
+        return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1f, 0.05f), CapsuleDirection2D.Horizontal, 0, groundLayer);
+    }
+
+    void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        facingRight = !facingRight;
     }
 }
